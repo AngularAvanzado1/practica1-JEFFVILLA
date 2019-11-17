98798b2fb021934fc2332deb332460519c35ad03
jeffvilla <csjeffri1994@gmail.com>
2019-11-17 23:35:52 +0100

    PWA, Cache Strategy, Api Cache

--- practica-final/apps/geographic-regions/src/app/app.module.ts
+++ practica-final/apps/geographic-regions/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@practica-final/layout';
 import { MaterialModule } from '@practica-final/material';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -44,7 +46,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     LayoutModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
